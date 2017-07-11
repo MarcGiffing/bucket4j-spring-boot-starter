70f1f2af24d5e8464a1b904ea17cd5c77df7156e
marcgiffing <marcgiffing@web.de>
2017-07-11 21:09:38 +0200

    some changes
    - increase capacity from 5 to 10
    - mark maven dependencies as provided

--- bucket4j-spring-boot-starter-example-ehcache/src/main/java/com/giffing/bucket4j/spring/boot/starter/examples/ehcache/users/UserController.java
+++ bucket4j-spring-boot-starter-example-ehcache/src/main/java/com/giffing/bucket4j/spring/boot/starter/examples/ehcache/users/UserController.java
@@ -2,17 +2,16 @@ package com.giffing.bucket4j.spring.boot.starter.examples.ehcache.users;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/")
 public class UserController {
 
-	@GetMapping("{id}")
-	public ResponseEntity<User> getById(@PathVariable("id") Long id) {
-		return ResponseEntity.ok(User.builder().firstname("Marc").lastname("Giffing").build());
+	@GetMapping
+	public ResponseEntity<User> user() {
+		return ResponseEntity.ok(User.builder().firstname("Frodo").lastname("Beutlin").build());
 	}
 	
 }
