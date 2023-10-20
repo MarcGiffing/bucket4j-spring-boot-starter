957e5b920c431fb2f2f497eb24e2833d231596db
Edwin Heuver <e.heuver@emagiz.com>
2023-10-20 16:58:08 +0200

    Fix typo

--- bucket4j-spring-boot-starter/src/main/java/com/giffing/bucket4j/spring/boot/starter/exception/DuplicateBandwidthIDException.java
+++ bucket4j-spring-boot-starter/src/main/java/com/giffing/bucket4j/spring/boot/starter/exception/DuplicateBandwidthIDException.java
@@ -7,6 +7,6 @@ public class DuplicateBandwidthIDException extends Bucket4jGeneralException {
     private static final long serialVersionUID = 1L;
 
     public DuplicateBandwidthIDException(String filterID, String bandwidthId) {
-        super("Duplicate bandWidth id '" + bandwidthId + "' detected in filter '" + filterID + ".");
+        super("Duplicate bandWidth id '" + bandwidthId + "' detected in filter '" + filterID + "'.");
     }
 }
