16b618692d437ffe6340f271f833efdb95c56414
Edwin Heuver <e.heuver@emagiz.com>
2023-11-10 16:04:21 +0100

    implement filter versioning

--- bucket4j-spring-boot-starter-context/src/main/java/com/giffing/bucket4j/spring/boot/starter/context/properties/Bucket4JConfiguration.java
+++ bucket4j-spring-boot-starter-context/src/main/java/com/giffing/bucket4j/spring/boot/starter/context/properties/Bucket4JConfiguration.java
@@ -5,10 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.micrometer.common.util.StringUtils;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 import org.springframework.core.Ordered;
 
@@ -43,7 +42,6 @@ public class Bucket4JConfiguration {
 	
 	/**
 	 * The URL to which the filter should be registered
-	 * 
 	 */
 	@NotBlank
 	private String url = ".*";
@@ -89,11 +87,41 @@ public class Bucket4JConfiguration {
 
 	private Metrics metrics = new Metrics();
 
+	/**
+	 * This identifier is used for saving and retrieving configurations in the cache.
+	 * Setting the id is mandatory when configuration caching is enabled.
+	 */
 	private String id;
 
 	public void setId(String id) {
 		if(!StringUtils.isBlank(id)){
 			this.id = id.trim();
 		}
+	}
+
+	/**
+	 * This version number is intended to be managed by the application.properties and for configuration changes by external systems during runtime.
+	 * The version is only relevant for filters that hava their identifier set, and defaults to 1 if not configured.
+	 *
+	 * The value is maxed at 92 million, since it is combined with the minor version into a single Long and cannot exceed the Long max value.
+	 */
+	@Min(1)
+	@Max(92000000L)
+	private long majorVersion = 1;
+
+	/**
+	 * This version number is intended for internal configuration updates during runtime, for example based on CPU-usage of the system,
+	 * and usually does not need to be configured in the application.properties.
+	 * The version is only relevant for filters that hava their identifier set, and defaults to 1 if not configured.
+	 *
+	 * The value is maxed at 99 billion, since it is combined with the major version into a single Long version and cannot exceed the Long max value.
+	 */
+	@Min(1)
+	@Max(99999999999L)
+	private long minorVersion = 1;
+
+	@JsonIgnore
+	public long getBucket4JVersionNumber (){
+		return (majorVersion * 100000000000L) + minorVersion;
 	}
 }
