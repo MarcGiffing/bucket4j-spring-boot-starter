28ccdad82cfe3e489c1691ea57510a954c750533
Edwin Heuver <e.heuver@emagiz.com>
2023-11-20 11:55:49 +0100

    add noArgsConstructor

--- bucket4j-spring-boot-starter/src/main/java/com/giffing/bucket4j/spring/boot/starter/config/cache/CacheUpdateEvent.java
+++ bucket4j-spring-boot-starter/src/main/java/com/giffing/bucket4j/spring/boot/starter/config/cache/CacheUpdateEvent.java
@@ -2,8 +2,10 @@ package com.giffing.bucket4j.spring.boot.starter.config.cache;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class CacheUpdateEvent <K,V> {
 	private K key;
