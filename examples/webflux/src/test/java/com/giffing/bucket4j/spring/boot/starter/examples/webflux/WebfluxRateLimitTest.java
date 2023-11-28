89af87e5e8620ebe2cc533f62ce524c94f5a1859
Edwin Heuver <e.heuver@emagiz.com>
2023-11-28 16:23:18 +0100

    add filter config replacement testcase for the webflux demo project

--- examples/webflux/src/test/java/com/giffing/bucket4j/spring/boot/starter/examples/webflux/WebfluxRateLimitTest.java
+++ examples/webflux/src/test/java/com/giffing/bucket4j/spring/boot/starter/examples/webflux/WebfluxRateLimitTest.java
@@ -3,22 +3,33 @@ package com.giffing.bucket4j.spring.boot.starter.examples.webflux;
 import java.util.Collections;
 import java.util.stream.IntStream;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.giffing.bucket4j.spring.boot.starter.context.properties.Bucket4JBootProperties;
+import com.giffing.bucket4j.spring.boot.starter.context.properties.Bucket4JConfiguration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @ActiveProfiles("webflux") // Like this
+@TestPropertySource(properties = {"bucket4j.filter-config-caching-enabled=true"})
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class WebfluxRateLimitTest {
 
 	@Autowired
     ApplicationContext context;
 
+	@Autowired
+	Bucket4JBootProperties properties;
+
+	private final ObjectMapper objectMapper = new ObjectMapper();
     WebTestClient rest;
 
     @BeforeEach
@@ -30,6 +41,7 @@ class WebfluxRateLimitTest {
     }
 
 	@Test
+	@Order(1)
 	void helloTest() throws Exception {
 		String url = "/hello";
 		IntStream.rangeClosed(1, 5)
@@ -44,6 +56,7 @@ class WebfluxRateLimitTest {
 
 	
 	@Test
+	@Order(1)
 	void worldTest() throws Exception {
 		String url = "/world";
 		IntStream.rangeClosed(1, 10)
@@ -56,7 +69,34 @@ class WebfluxRateLimitTest {
 		
 			blockedWebRequestDueToRateLimit(url);
 		}
-	
+
+	@Test
+	@Order(2)
+	void replaceConfigTest() throws Exception {
+		int newFilterCapacity = 1000;
+
+		//get the /hello filter
+		Bucket4JConfiguration filter = properties.getFilters().stream().filter(x -> "/hello".matches(x.getUrl())).findFirst().orElse(null);
+		assert filter != null;
+
+		//clone the filter so we don't modify the original, increase the version and set the new capacity for all bandwidths
+		Bucket4JConfiguration clone = objectMapper.readValue(objectMapper.writeValueAsString(filter),Bucket4JConfiguration.class);
+		clone.setMajorVersion(clone.getMajorVersion() + 1);
+		clone.getRateLimits().forEach(rl -> {
+			rl.getBandwidths().forEach(bw -> bw.setCapacity(newFilterCapacity));
+		});
+
+		//update the filter cache
+		String url = "/filters/".concat(clone.getId());
+		rest.post().uri(url).bodyValue(clone).exchange().expectStatus().isOk();
+
+		//Short sleep to allow the cacheUpdateListeners to update the filter configuration
+		Thread.sleep(100);
+
+		//validate that the new capacity is applied to requests
+		successfulWebRequest("/hello", newFilterCapacity-1);
+	}
+
 	private void successfulWebRequest(String url, Integer remainingTries) {
 		rest
 			.get()
