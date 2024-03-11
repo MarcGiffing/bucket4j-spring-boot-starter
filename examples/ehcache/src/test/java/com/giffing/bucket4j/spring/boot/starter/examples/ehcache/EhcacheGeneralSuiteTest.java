0860e0c294880b531c6cc7b7fcedb8ab45b87227
Marc Giffing <MarcGiffing@users.noreply.github.com>
2024-03-11 20:12:50 +0100

    Support for Method level @RateLimiting annoation #250

--- examples/ehcache/src/test/java/com/giffing/bucket4j/spring/boot/starter/examples/ehcache/EhcacheGeneralSuiteTest.java
+++ examples/ehcache/src/test/java/com/giffing/bucket4j/spring/boot/starter/examples/ehcache/EhcacheGeneralSuiteTest.java
@@ -1,6 +1,5 @@
 package com.giffing.bucket4j.spring.boot.starter.examples.ehcache;
 
-import com.giffing.bucket4j.spring.boot.starter.general.tests.filter.method.MethodTestSuite;
 import com.giffing.bucket4j.spring.boot.starter.general.tests.filter.servlet.ServletTestSuite;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
@@ -8,7 +7,6 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @SelectClasses({
         ServletTestSuite.class,
-        MethodTestSuite.class,
 })
 public class EhcacheGeneralSuiteTest {
 }
