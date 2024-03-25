255411a027363f09fd2dd2fdb060d7d502b7d304
Marc Giffing <MarcGiffing@users.noreply.github.com>
2024-03-25 14:20:15 +0100

    #276 Start-up crash when disabled via property

--- examples/general-tests/src/main/java/com/giffing/bucket4j/spring/boot/starter/general/tests/method/method/MethodTestSuite.java
+++ examples/general-tests/src/main/java/com/giffing/bucket4j/spring/boot/starter/general/tests/method/method/MethodTestSuite.java
@@ -8,7 +8,8 @@ import org.junit.platform.suite.api.Suite;
 @SelectClasses({
         MethodRateLimitTest.class,
         NoCacheFoundTest.class,
-        RateLimitConfigurationStartupFailuresTest.class
+        RateLimitConfigurationStartupFailuresTest.class,
+        Bucket4jDisabledTest.class
 })
 public class MethodTestSuite {
 }
