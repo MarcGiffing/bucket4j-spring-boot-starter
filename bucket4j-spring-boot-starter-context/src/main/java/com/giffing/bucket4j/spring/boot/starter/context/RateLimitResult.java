b2e3c2fb13923ac911e179e09d866880e60b6391
Marc Giffing <MarcGiffing@users.noreply.github.com>
2025-05-24 21:17:08 +0200

    remove useless @NonNull(lombok) annotations for primitive types

--- bucket4j-spring-boot-starter-context/src/main/java/com/giffing/bucket4j/spring/boot/starter/context/RateLimitResult.java
+++ bucket4j-spring-boot-starter-context/src/main/java/com/giffing/bucket4j/spring/boot/starter/context/RateLimitResult.java
@@ -2,7 +2,6 @@ package com.giffing.bucket4j.spring.boot.starter.context;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.NonNull;
 
 /**
  * This data class holds the information of a rate limit check.
@@ -14,7 +13,6 @@ public class RateLimitResult {
     /**
      * If the request was only for estimation without consuming any tokens.
      */
-    @NonNull
     private final boolean estimation;
 
     /**
@@ -22,24 +20,20 @@ public class RateLimitResult {
      * <p>
      * If {@link #estimation} is true no tokens are consumed.
      */
-    @NonNull
     private final boolean consumed;
 
     /**
      * The number of tokens that remains until rate limit is executed.
      */
-    @NonNull
     private final long remainingTokens;
 
     /**
      * The time in nanoseconds until the next tokens will be refilled.
      */
-    @NonNull
     private final long nanosToWaitForRefill;
 
     /**
      * The time in nanoseconds until the tokens are refilled to its maximum.
      */
-    @NonNull
     private final long nanosToWaitForReset;
 }
