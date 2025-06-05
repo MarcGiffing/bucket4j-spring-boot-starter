dc877355f7a4773ed2e3bb0e55d8e391a16bc43e
amotov <alexandr.motov@gmail.com>
2025-06-05 23:33:27 +0500

    Fixed post execute condition (#409)
    * Fixed post execute condition
    
    * Refactored and added test
    
    * Reverted some changes
    
    ---------
    
    Co-authored-by: amotov <alexandr.motov@kaspi.kz>

--- bucket4j-spring-boot-starter/src/test/java/com/giffing/bucket4j/spring/boot/starter/webflux/WebfluxRateLimitFilterTest.java
+++ bucket4j-spring-boot-starter/src/test/java/com/giffing/bucket4j/spring/boot/starter/webflux/WebfluxRateLimitFilterTest.java
@@ -1,9 +1,6 @@
 package com.giffing.bucket4j.spring.boot.starter.webflux;
 
-import com.giffing.bucket4j.spring.boot.starter.context.RateLimitCheck;
-import com.giffing.bucket4j.spring.boot.starter.context.RateLimitConditionMatchingStrategy;
-import com.giffing.bucket4j.spring.boot.starter.context.RateLimitResult;
-import com.giffing.bucket4j.spring.boot.starter.context.RateLimitResultWrapper;
+import com.giffing.bucket4j.spring.boot.starter.context.*;
 import com.giffing.bucket4j.spring.boot.starter.context.properties.FilterConfiguration;
 import com.giffing.bucket4j.spring.boot.starter.filter.reactive.ReactiveRateLimitException;
 import com.giffing.bucket4j.spring.boot.starter.filter.reactive.webflux.WebfluxWebFilter;
@@ -13,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
@@ -22,9 +20,12 @@ import reactor.core.publisher.Mono;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -37,6 +38,8 @@ class WebfluxRateLimitFilterTest {
 	private RateLimitCheck<ServerHttpRequest> rateLimitCheck2;
 	private RateLimitCheck<ServerHttpRequest> rateLimitCheck3;
 
+	private PostRateLimitCheck<ServerHttpRequest, ServerHttpResponse> postRateLimitCheck;
+
 	private ServerWebExchange exchange;
 	private WebFilterChain chain;
 
@@ -48,6 +51,8 @@ class WebfluxRateLimitFilterTest {
 		rateLimitCheck1 = mock(RateLimitCheck.class);
 		rateLimitCheck2 = mock(RateLimitCheck.class);
 		rateLimitCheck3 = mock(RateLimitCheck.class);
+
+		postRateLimitCheck = mock(PostRateLimitCheck.class);
 
 		exchange = Mockito.mock(ServerWebExchange.class);
 
@@ -64,6 +69,9 @@ class WebfluxRateLimitFilterTest {
 
 		configuration = new FilterConfiguration<>();
 		configuration.setRateLimitChecks(Arrays.asList(rateLimitCheck1, rateLimitCheck2, rateLimitCheck3));
+		configuration.setPostRateLimitChecks(
+				Collections.singletonList(
+						postRateLimitCheck));
 		configuration.setUrl(".*");
 		filter = new WebfluxWebFilter(configuration);
 	}
@@ -134,14 +142,86 @@ class WebfluxRateLimitFilterTest {
 		verify(rateLimitCheck3, times(0)).rateLimit(any(), any());
 	}
 
+	@Test
+	void should_execute_rate_limit_check_after_main_filter() {
+		configuration.setStrategy(RateLimitConditionMatchingStrategy.ALL);
+
+		rateLimitConfig(10L, rateLimitCheck1);
+		rateLimitConfig(10L, rateLimitCheck2);
+		rateLimitConfig(10L, rateLimitCheck3);
+
+		var httpHeaders = Mockito.mock(HttpHeaders.class);
+		when(serverHttpResponse.getHeaders()).thenReturn(httpHeaders);
+		when(serverHttpResponse.getStatusCode()).thenReturn(HttpStatusCode.valueOf(200));
+
+		when(chain.filter(exchange))
+				.thenReturn(
+						Mono.defer(() -> {
+							when(serverHttpResponse.getStatusCode())
+									.thenReturn(HttpStatusCode.valueOf(401));
+							return Mono.empty();
+						}));
+
+		var statusCode =
+                new AtomicReference<>(
+                        serverHttpResponse.getStatusCode());
+		configurePostRateLimit(
+				postRateLimitCheck,
+				Mono.defer(() -> {
+							statusCode.set(
+									serverHttpResponse.getStatusCode());
+							return Mono.just(
+									createRateLimitResult(0L));
+						}
+						));
+
+		filter.filter(exchange, chain).block();
+
+		Assertions.assertEquals(
+				HttpStatusCode.valueOf(401),
+				statusCode.get());
+	}
+
+	public static <T> Supplier<CompletableFuture<T>> createLazyFuture(Supplier<Mono<T>> monoSupplier) {
+		return () -> monoSupplier.get().toFuture();
+	}
+
 	private void rateLimitConfig(Long remainingTokens, RateLimitCheck<ServerHttpRequest> rateLimitCheck) {
 		RateLimitResultWrapper consumptionHolder = Mockito.mock(RateLimitResultWrapper.class);
+		RateLimitResult rateLimitResult = createRateLimitResult(remainingTokens);
+		when(consumptionHolder.getRateLimitResultCompletableFuture())
+				.thenReturn(
+						CompletableFuture.completedFuture(rateLimitResult));
+		when(rateLimitCheck.rateLimit(any(), any())).thenReturn(consumptionHolder);
+	}
+
+	private void configurePostRateLimit(
+			PostRateLimitCheck<ServerHttpRequest, ServerHttpResponse> rateLimitCheck,
+			Long remainingTokens) {
+		configurePostRateLimit(
+				rateLimitCheck,
+					Mono.just(
+							createRateLimitResult(
+									remainingTokens)));
+	}
+
+	private void configurePostRateLimit(
+			PostRateLimitCheck<ServerHttpRequest, ServerHttpResponse> rateLimitCheck,
+			Mono<RateLimitResult> result) {
+		RateLimitResultWrapper consumptionHolder = Mockito.mock(RateLimitResultWrapper.class);
+
+		when(consumptionHolder.getRateLimitResultCompletableFuture())
+				.thenAnswer(invocation -> result.toFuture());
+		when(rateLimitCheck.rateLimit(any(), any())).thenReturn(consumptionHolder);
+	}
+
+	private RateLimitResult createRateLimitResult(
+			Long remainingTokens) {
 		RateLimitResult rateLimitResult = Mockito.mock(RateLimitResult.class);
 		when(rateLimitResult.isConsumed()).thenReturn(remainingTokens > 0);
 		when(rateLimitResult.getRemainingTokens()).thenReturn(remainingTokens);
-		when(consumptionHolder.getRateLimitResultCompletableFuture())
-				.thenReturn(CompletableFuture.completedFuture(rateLimitResult));
-		when(rateLimitCheck.rateLimit(any(), any())).thenReturn(consumptionHolder);
+
+		return rateLimitResult;
 	}
 
 }
