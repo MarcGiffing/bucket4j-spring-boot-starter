7d8b21b6050d033d8cef1d09dc246735d3e27307
Marc Giffing <MarcGiffing@users.noreply.github.com>
2026-03-06 17:00:14 +0100

    #488 restructure code - remove allow-bean-definition-overriding

--- context/src/main/java/com/giffing/bucket4j/spring/boot/starter/context/converter/JacksonEnumDeserializer.java
+++ context/src/main/java/com/giffing/bucket4j/spring/boot/starter/context/converter/JacksonEnumDeserializer.java
@@ -1,16 +1,13 @@
 package com.giffing.bucket4j.spring.boot.starter.context.converter;
 
-import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
-import java.io.IOException;
-
 public class JacksonEnumDeserializer extends JsonDeserializer<Enum<?>> {
 
     @Override
-    public Enum<?> deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+    public Enum<?> deserialize(JsonParser p, DeserializationContext ctxt) {
         return null;
     }
 }
